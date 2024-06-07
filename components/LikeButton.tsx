2a30a4e8243b90fd236c8d63c0a6e7c042490559
Ana MacBook <anac2828@yahoo.com>
2024-06-07 15:30:01 -0700

    like button

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -4,20 +4,53 @@ import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import { useSessionContext } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { AiFillHeart, AiOutlineHeart } from 'react-icons/ai';
 
 interface LikeButtonProps {
   songId: string;
 }
 
-const LikeButton: React.FC<LikeButtonProps> = () => {
+const LikeButton: React.FC<LikeButtonProps> = ({ songId }) => {
   const router = useRouter();
   const { supabaseClient } = useSessionContext();
   const authMotal = useAuthModal();
   const { user } = useUser();
 
-  const [liked, setLiked] = useState(false);
-  return <div>Like button</div>;
+  const [isLiked, setIsLiked] = useState(false);
+
+  // Fetch liked songs
+  useEffect(() => {
+    if (!user?.id) return;
+
+    const fetchData = async () => {
+      const { data, error } = await supabaseClient
+        .from('like_songs')
+        .select('*')
+        .eq('user_id', user.id)
+        .eq('song_id', songId)
+        .single();
+
+      if (!error && data) setIsLiked(true);
+    };
+
+    fetchData();
+  }, [songId, user?.id, supabaseClient]);
+
+  const Icon = isLiked ? AiFillHeart : AiOutlineHeart;
+
+  const handleLike = async () => {
+    if (!user) authModal.onOpend();
+    if (isLiked) {
+      const { error } = supabaseClient.from('likedSongs');
+    }
+  };
+
+  return (
+    <button className='hover:opacity-75 trasition' onClick={handleLike}>
+      <Icon color={isLiked ? '#22c55e' : 'white'} size={25} />
+    </button>
+  );
 };
 
 export default LikeButton;
