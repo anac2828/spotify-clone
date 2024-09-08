5bc2c5f88381e466d5300e00d5abc0f14b829ef9
anac2828 <anac2828@yahoo.com>
2024-09-07 22:00:29 -0700

    wip: susbription modal

--- components/SubscribeModal.tsx
+++ components/SubscribeModal.tsx
@@ -1,8 +1,53 @@
 'use client';
+import React, { useState } from 'react';
+import { Price, ProductWithPrice } from '@/types';
+import { useUser } from '@/hooks/useUser';
 import Modal from './Modal';
+import Button from './Button';
 
-function SubscribeModal() {
-  const content = <div className='text-center'>No products available.</div>;
+interface SubscribeModalProps {
+  products: ProductWithPrice[];
+}
+
+const formatPrice = (price: Price) => {
+  const priceString = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: price.currency,
+    minimumFractionDigits: 0,
+  }).format((price?.unit_amount || 0) / 100);
+  return priceString;
+};
+
+const SubscribeModal: React.FC<SubscribeModalProps> = ({ products }) => {
+  const { user, isLoading, subscription } = useUser();
+  const [priceIdLoading, setPriceIdLoading] = useState<string>();
+
+  const handleCheckout = async (price: Price) => {};
+
+  let content = <div className='text-center'>No products available.</div>;
+
+  if (products.length) {
+    content = (
+      <div>
+        {products.map((product) => {
+          if (!product.prices?.length) {
+            return <div key={product.id}>No prices available</div>;
+          }
+
+          return product.prices.map((price) => (
+            <Button
+              key={price.id}
+              onClick={() => handleCheckout(price)}
+              disabled={isLoading || price.id === priceIdLoading}
+              className='mb-4'
+            >
+              {`Subscribe for ${formatPrice(price)} a ${price.interval}`}
+            </Button>
+          ));
+        })}
+      </div>
+    );
+  }
   return (
     <Modal
       title='Only for premium users'
@@ -10,9 +55,9 @@ function SubscribeModal() {
       isOpen
       onChange={() => {}}
     >
-      Subscriptions
+      {content}
     </Modal>
   );
-}
+};
 
 export default SubscribeModal;
